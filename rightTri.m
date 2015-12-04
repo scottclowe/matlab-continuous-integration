c21e29f19fc23ef7d4abb3e3c79df2e87e1b2a0c
Scott Lowe <scott.code.lowe@gmail.com>
2015-12-04 14:20:13 +0000

    Add Example built-in script unit test for MATLAB
    Example taken from MathWorks
    https://uk.mathworks.com/help/matlab/matlab_prog/write-script-based-unit-tests.html

---
+++ rightTri.m
@@ -0,0 +1,12 @@
+% Copyright 2015 The MathWorks, Inc.
+
+function angles = rightTri(sides)
+
+A = atand(sides(1)/sides(2));
+B = atand(sides(2)/sides(1));
+hypotenuse = sides(1)/sind(A);
+C = asind(hypotenuse*sind(A)/sides(1));
+
+angles = [A B C];
+
+end
