f3c730abf6281bc2d794b6bb77d66e621c2eda48
Scott Lowe <scott.code.lowe@gmail.com>
2015-12-04 20:27:18 +0000

    Move functions and tests into folders
    and update runAllTests to reflect the new locations.

--- runAllTests.m
+++ runAllTests.m
@@ -1,7 +1,9 @@
+addpath(genpath('funcs'));
+
 if isoctave
     % Octave
-    runtests('./');
+    runtests('./tests_octave');
 else
     % Matlab
-    result = runtests(pwd, 'Recursively', true);
+    result = runtests(fullfile(pwd, 'funcs'), 'Recursively', true);
 end
