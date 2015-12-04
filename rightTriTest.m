1d98901bc0ebe0e4b279cadfc84ca6853f381c40
Scott Lowe <scott.code.lowe@gmail.com>
2015-12-04 14:48:36 +0000

    Add tol so tests pass

--- rightTriTest.m
+++ rightTriTest.m
@@ -7,6 +7,9 @@ triIso = [4 4];
 tri306090 = [2 2*sqrt(3)];
 triSkewed = [1 1500];
 
+% Define an absolute tolerance
+tol = 1e-10; 
+ 
 % preconditions
 angles = rightTri(tri);
 assert(angles(3) == 90,'Fundamental problem: rightTri not producing right triangle')
@@ -31,12 +34,12 @@ assert(angles(1) == angles(2))
  
 %% Test 3: 30-60-90 triangle
 angles = rightTri(tri306090);
-assert(angles(1) == 30)
-assert(angles(2) == 60)
-assert(angles(3) == 90)
+assert(abs(angles(1)-30) <= tol)
+assert(abs(angles(2)-60) <= tol)
+assert(abs(angles(3)-90) <= tol)
 
 %% Test 4: Small angle approximation
 angles = rightTri(triSkewed);
 smallAngle = (pi/180)*angles(1); % radians
 approx = sin(smallAngle);
-assert(approx == smallAngle, 'Problem with small angle approximation')
+assert(abs(approx-smallAngle) <= tol, 'Problem with small angle approximation')
