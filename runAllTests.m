b5973485fc1822c21bdf02b2c0bac2e90dbb4131
Scott Lowe <scott.code.lowe@gmail.com>
2015-12-05 17:03:54 +0000

    Reformat test script to fail on failure

--- runAllTests.m
+++ runAllTests.m
@@ -1,18 +1,20 @@
-% NB: Need to use fullfile when adding paths so that Octave plays
-% nicely with MOxUnit
-addpath(genpath(fullfile(pwd,'funcs')));
+function result = runAllTests()
+    % NB: Need to use fullfile when adding paths so that Octave plays
+    % nicely with MOxUnit
+    addpath(genpath(fullfile(pwd,'funcs')));
 
-if true
-    % Do nothing
-elseif isoctave
-    % Octave
-    disp('Builtin octave test suite');
-    runtests('./tests_octave');
-else
-    % Matlab
-    disp('Builtin Matlab test suite');
-    result = runtests(fullfile(pwd, 'funcs'), 'Recursively', true);
-end
+    if true
+        % Do nothing
+    elseif isoctave
+        % Octave
+        disp('Builtin octave test suite');
+        runtests('./tests_octave');
+    else
+        % Matlab
+        disp('Builtin Matlab test suite');
+        result = runtests(fullfile(pwd, 'funcs'), 'Recursively', true);
+    end
 
-disp('MOxUnit test suite');
-moxunit_runtests tests_moxunit -verbose -junitxml testresults.xml;
+    disp('MOxUnit test suite');
+    result = moxunit_runtests('tests_moxunit', '-verbose', ...
+                              '-junitxml', 'testresults.xml');
