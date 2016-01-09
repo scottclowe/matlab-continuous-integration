1e74b2bc4cb33e8198bae3657f444474152bea5a
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-09 13:37:43 +0000

    Tidy up isoctave, add comments

--- isoctave.m
+++ isoctave.m
@@ -1,15 +1,19 @@
-function tf = isoctave()
 %ISOCTAVE  Determine if the environment is Octave
 %   ISOCTAVE() true if the operating environment is Octave, otherwise
 %   it returns false, indicating the environment is something else
 %   (MATLAB, Scilab, FreeMat, etc).
+function tf = isoctave()
 
-persistent tf_cached;
+    % Cache the value because it can't change
+    persistent tf_cached;
 
-if isempty(tf_cached)
-  tf_cached = exist('OCTAVE_VERSION','builtin') ~= 0;
-end
+    % If this is the first call, check if we are in octave.
+    % We can tell because the 'OCTAVE_VERSION' function will exist.
+    if isempty(tf_cached)
+      tf_cached = exist('OCTAVE_VERSION','builtin') ~= 0;
+    end
 
-tf = tf_cached;
+    % Set the return value equal to the cached value
+    tf = tf_cached;
 
 end
