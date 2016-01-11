b773877164f24b13dc25ea3b4dd4ff74b950b9d4
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-11 20:28:04 +0000

    Refactor and extend tests of rectifiedCubic.m

--- tests/rectifiedCubic_test.m
+++ tests/rectifiedCubic_test.m
@@ -1,61 +1,64 @@
 % Tests for rectifiedCubic, implemented with the MOxUnit framework
 
 function test_suite=rectifiedCubic_test()
-    initTestSuite
+    % Top level function should call initTestSuite and return the
+    % variable test_suite (which initTestSuite will put into the
+    % workspace for us).
+    initTestSuite;
 end
 
-function test_rectifiedCubic_input_handling()
-    assertExceptionThrown(@()rectifiedCubic('string'), '');
+function test_input_handling()
+    % Check that an error is thrown for a bad input. The second
+    % parameter is the expected error message, which in our case is
+    % omitted because rectifiedCubic throws the error with a call to
+    % `assert()`, not `error()`.
+    % Throw an error if input is a string.
+    assertExceptionThrown(@()rectifiedCubic('foobar'), '');
 end
 
-function test_rectifiedCubic_main()
-    assert(isequal(8, rectifiedCubic(2)))
-    assert(isequal(0, rectifiedCubic(-2)))
-    assert(isequal(27, rectifiedCubic(3)))
+function test_integer()
+    % Check the function works as expected for integer inputs.
+    assertEqual(rectifiedCubic(2), 8);
+    assertEqual(rectifiedCubic(-2), 0);
+    assertEqual(rectifiedCubic(3), 27);
+    assertEqual(rectifiedCubic(int8(5)), int8(125));
 end
 
-function ignored_test_rectifiedCubic_fails()
-    % This is ok
-    assert(isequal(27, rectifiedCubic(3)))
-    % This is not and should fail
-    assert(isequal(27, rectifiedCubic(-3)))
-    % This is ok
-    assert(isequal(1, rectifiedCubic(1)))
-    % This is not and should fail
-    assert(isequal(1, rectifiedCubic(-1)))
+function test_float()
+    % Check the function works as expected for float inputs.
+    % We need to use assertElementsAlmostEqual due to the very real
+    % possibility of a floating point inaccuracy.
+    assertElementsAlmostEqual(rectifiedCubic(1.2), 1.728);
+    assertElementsAlmostEqual(rectifiedCubic(-1.2), 0);
 end
 
-function test_rectifiedCubic_skips()
-    % This is ok
-    assert(isequal(27, rectifiedCubic(3)))
-    % This is not and should fail
-    moxunit_throw_test_skipped_exception('hmm this shouldnt work');
-    assert(isequal(27, rectifiedCubic(-3)))
-    % This is ok
-    assert(isequal(1, rectifiedCubic(1)))
-    % This is not and should fail
-    moxunit_throw_test_skipped_exception('hmm nor should this');
-    assert(isequal(1, rectifiedCubic(-1)))
+function test_vector()
+    % Test empty vector input
+    assertEqual(rectifiedCubic([]), []);
+    % Check the function works for row vectors
+    assertEqual(rectifiedCubic([0, 2, -2]), [0, 8, 0]);
+    % and for column vectors too
+    assertEqual(rectifiedCubic([0; 2; -2]), [0; 8; 0]);
+    % Need to use assertElementsAlmostEqual when working with colon
+    % because the elements it generates are not precisely integers.
+    assertElementsAlmostEqual(rectifiedCubic(-3:3), [0, 0, 0, 0, 1, 8, 27]);
 end
 
-function test_skip_success()
-    moxunit_throw_test_skipped_exception('this might not be skipped');
-    assertTrue(true);
+function test_nan_skip()
+    % We can skip a test function with this MOxUnit utility.
+    moxunit_throw_test_skipped_exception('Skip test with NaN input.');
+    % If we use the builtin `==` or `isequal` functions, they will
+    % say NaNs are unequal.
+    assertTrue(rectifiedCubic(NaN)==NaN);
+    assertTrue(isequal(rectifiedCubic(NaN), NaN));
 end
 
-function test_timing()
-    pause(0.5);
-    assertTrue(true);
-end
-
-function ignored_test_string_equality_failure()
-    assertEqual('foo','bar');
-end
-
-function ignored_test_cell_equality_failure()
-    assertEqual({},{'bar'});
-end
-
-function ignored_test_class_equality_failure()
-    assertEqual('foo',{'bar'});
+function test_nan()
+    % We can use the MOxUnit function `assertEqual` to compare two
+    % NaNs with each other.
+    assertEqual(rectifiedCubic(NaN), NaN);
+    % Alternatively, we could use `isnan` and wrap it in assertTrue,
+    % but this is less elegant and will lead to worse feedback from
+    % MOxUnit in the event of an error.
+    assertTrue(isnan(rectifiedCubic(NaN)));
 end
