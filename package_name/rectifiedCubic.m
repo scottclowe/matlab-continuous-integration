8b73097466fe9f94ab84003ae0c750ec54aa7516
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-12 12:35:10 +0000

    Improve rectifiedCubic documentation

--- package_name/rectifiedCubic.m
+++ package_name/rectifiedCubic.m
@@ -1,6 +1,11 @@
+%rectifiedCubic    Rectified value of the cube of the input.
+%   Y = rectifiedCubic(X) returns the element-wise rectified-cubic value
+%   of X. For positive elements, this is the cube of the element. For
+%   negative elements, this is 0.
+%
+%   NaN values in the input are preserved in the output.
+
 function y = rectifiedCubic(x)
-    % Returns the rectified value of the cube of X.
-    % If X is positive, this is the cube of X, if X is negative it is 0.
 
     assert(isnumeric(x), 'Input must be numeric');
 
