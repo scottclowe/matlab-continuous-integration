061b4218508dd5e59dafbd75418b4354b63605a9
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-12 12:34:50 +0000

    Indent rectifiedCubic function contents

--- package_name/rectifiedCubic.m
+++ package_name/rectifiedCubic.m
@@ -1,9 +1,9 @@
 function y = rectifiedCubic(x)
-% Returns the rectified value of the cube of X.
-% If X is positive, this is the cube of X, if X is negative it is 0.
+    % Returns the rectified value of the cube of X.
+    % If X is positive, this is the cube of X, if X is negative it is 0.
 
-assert(isnumeric(x), 'Input must be numeric');
+    assert(isnumeric(x), 'Input must be numeric');
 
-y = max(0, x.^3);
+    y = max(0, x.^3);
 
 end
